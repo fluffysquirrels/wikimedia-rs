a5b8b118d09ba53b62c3478174f1aa3eb38e5185
Alex Helfet <alex.helfet@gmail.com>
2023-03-04 18:46:33 +0000

    Working download command.

---
+++ TODO.md
@@ -0,0 +1,32 @@
+# To do
+
+## Must do before publishing
+
+* Handle it gracefully in the cronjob when:
+    *  The status of the job is not "done" (e.g. still in
+       progress). At the moment the `download` subcommand just returns
+       an Err() with a message, which is really machine readable. Probably
+       return a custom `Error` struct with an `kind: ErrorKind` field.
+    *  Downloads fail. Retry automatically after a short delay or next
+       time the cronjob runs.
+* Download files with a temporary extension, then move them into place when done.
+* `--overwrite` argument for `download` subcommand.
+* Support logging as JSON.
+
+## Might do
+
+* Subcommand to list dumps
+* Subcommand to list versions
+* Subcommand to list jobs
+* Subcommand to list files for a job
+* More performant, elegant, featureful async streaming downloads.
+    * Progress bar
+    * Write while beginning next read.
+    * Configurable timeout
+    * Cancellation support
+* Specify a version to `download` subcommand with `--version`, still pick the latest by default.
+* Avoid boilerplate to record context variables in `download` subcommand.
+    * Perhaps use `tracing::span` to record context variables, with
+      events setting their parent to that span
+* Unit tests for file relative URL validation
+* Validate dump name, job name to have no relative paths, path traversal.
--- src/commands/download.rs
+++ src/commands/download.rs
@@ -1,13 +1,18 @@
-// use anyhow::Context;
+use anyhow::Context;
 use crate::{
     args::CommonArgs,
     Result,
 };
 use regex::Regex;
+use serde::Deserialize;
+use sha1::{Sha1, Digest};
 use std::{
-    path::PathBuf,
+    collections::BTreeMap,
+    path::{Path, PathBuf},
     time::Duration,
 };
+use tokio::io::AsyncWriteExt;
+use tokio_stream::StreamExt;
 use tracing::Level;
 
 /// Download latest dump job files from Wikimedia
@@ -33,15 +38,48 @@ pub struct Args {
     /// The directory to download to.
     ///
     /// The dump files will be placed in a child directory of this.
-    /// With `--out-dir` set to `./out`, dump files will be like:
-    /// `./out/enwiki/20230301/enwiki-20230301-pages-articles.xml.bz2`
+    /// With `--out-dir` set to `./out`, dump file paths will be like:
+    /// `./out/enwiki/20230301/metacurrentdumprecombine/enwiki-20230301-pages-articles.xml.bz2`
     ///
     /// If not present tries to read the environment variable `WMD_OUT_DIR`.
     #[arg(long, env = "WMD_OUT_DIR")]
     out_dir: PathBuf,
 }
 
+#[derive(Debug, Deserialize)]
+struct DumpVersionStatus {
+    jobs: BTreeMap<String, JobStatus>,
+
+    #[allow(dead_code)] // Not used currently
+    version: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct JobStatus {
+    status: String,
+
+    #[allow(dead_code)] // Not used currently
+    updated: String,
+
+    #[serde(default)]
+    files: BTreeMap<String, FileMetadata>,
+}
+
+#[derive(Debug, Deserialize)]
+struct FileMetadata {
+    size: u64,
+    url: String,
+    sha1: Option<String>,
+
+    #[allow(dead_code)] // Not used currently
+    md5: Option<String>,
+}
+
+#[tracing::instrument(level = "trace")]
 pub async fn main(args: Args) -> Result<()> {
+    let dump_name = &*args.dump_name;
+    let job_name = &*args.job_name;
+
     let client = reqwest::ClientBuilder::new()
         .user_agent(concat!(
             env!("CARGO_PKG_NAME"),
@@ -51,10 +89,39 @@ pub async fn main(args: Args) -> Result<()> {
         // .gzip(true)
         // .timeout(...)
         .build()?;
-    let vers = get_dump_versions(&client, &*args.dump_name).await?;
+
+    let mut vers = get_dump_versions(&client, &*args.dump_name).await?;
+    if vers.is_empty() {
+        return Err(anyhow::Error::msg(format!("No versions found for dump {dump_name}")));
+    }
+    vers.sort();
+    // Re-bind as immutable.
+    let vers = vers;
+
+    let ver = vers.last().expect("vers not empty");
+
+    let ver_status = get_dump_version_status(&client, &*args.dump_name, ver).await?;
+
+    let Some(job_status) = ver_status.jobs.get(&*args.job_name) else {
+        return Err(anyhow::Error::msg(format!("No status found for job job_name={job_name} version={ver} dump_name={dump_name}")));
+    };
+
+    if tracing::enabled!(Level::TRACE) {
+        tracing::trace!(job_status = format!("{:#?}", job_status), "Job status");
+    }
+
+    if job_status.status != "done" {
+        return Err(anyhow::Error::msg(format!("Job status is not 'done' status={status} job_name={job_name} version={ver} dump_name={dump_name}", status = job_status.status)));
+    }
+
+    for file_meta in job_status.files.values() {
+        download_job_file(&client, &*args.out_dir, dump_name, ver, job_name, file_meta).await?;
+    }
+
     Ok(())
 }
 
+#[tracing::instrument(level = "trace", skip(client))]
 async fn get_dump_versions(client: &reqwest::Client, dump_name: &str) -> Result<Vec<String>> {
     let url = format!("https://dumps.wikimedia.org/{dump_name}/");
     let res = client.get(url.clone())
@@ -82,20 +149,18 @@ async fn get_dump_versions(client: &reqwest::Client, dump_name: &str) -> Result<
 
     let doc = scraper::Html::parse_document(&*res_text);
     if !doc.errors.is_empty() {
-        tracing::warn!(errors = format!("{:?}", doc.errors),
+        tracing::warn!(errors = ?doc.errors,
                        "dump versions body had HTML parse errors");
     }
 
-    let mut ret = Vec::<String>::new();
+    let mut vers = Vec::<String>::new();
 
     // TODO: Use lazy_static!
     let date_href_re = Regex::new(r"^(?P<date>\d{8})/$").expect("parse regex");
 
     for link in doc.select(&scraper::Selector::parse("a").expect("parse selector")) {
         let href = link.value().attr("href");
-        if tracing::enabled!(Level::TRACE) {
-            tracing::trace!(href = href, "dump versions link");
-        }
+        tracing::trace!(href = href, "dump versions link");
 
         let Some(href) = href else {
             continue;
@@ -105,13 +170,179 @@ async fn get_dump_versions(client: &reqwest::Client, dump_name: &str) -> Result<
             continue;
         };
 
-        ret.push(cap.name("date").expect("regex capture name").as_str().to_string());
+        vers.push(cap.name("date").expect("regex capture name").as_str().to_string());
     }
+
+    tracing::debug!(versions_count = vers.len(),
+                    "dump versions ret count");
 
     if tracing::enabled!(Level::TRACE) {
-        tracing::trace!(versions = format!("{:?}", ret),
-                       "dump versions ret");
+        tracing::trace!(versions = ?vers,
+                       "dump versions ret data");
     }
 
-    Ok(ret)
+    Ok(vers)
+}
+
+#[tracing::instrument(level = "trace", skip(client), ret)]
+async fn get_dump_version_status(
+    client: &reqwest::Client,
+    dump_name: &str,
+    ver: &str
+) -> Result<DumpVersionStatus> {
+
+    let url = format!("https://dumps.wikimedia.org/{dump_name}/{ver}/dumpstatus.json");
+    let res = client.get(url.clone())
+                    .timeout(Duration::from_secs(10))
+                    .send()
+                    .await?;
+    let res_code = res.status();
+    let res_code_int = res_code.as_u16();
+    let res_code_str = res_code.canonical_reason().unwrap_or("");
+    tracing::info!(url = url.clone(),
+                   response_code = res_code_int,
+                   response_code_str = res_code_str,
+                   "GET dump version status");
+
+    let res_text = res.text().await?;
+    if tracing::enabled!(Level::TRACE) {
+        tracing::trace!(body_text = res_text.clone(),
+                       "GET dump version status body");
+    }
+
+    if !res_code.is_success() {
+        return Err(anyhow::Error::msg(
+            format!("HTTP response error fetching dump version status url={url} response_code={res_code_int} response_code_str={res_code_str}")));
+    }
+
+    let status: DumpVersionStatus = serde_json::from_str(&*res_text)
+        .with_context(|| format!("Getting dump version status url={url}"))?;
+
+    Ok(status)
+}
+
+#[tracing::instrument(level = "trace", skip(client))]
+async fn download_job_file(
+    client: &reqwest::Client,
+    out_dir: &Path,
+    dump_name: &str,
+    ver: &str,
+    job_name: &str,
+    file_meta: &FileMetadata
+) -> Result<()> {
+    let mut rel_segments = file_meta.url.split('/');
+    let Some(first) = rel_segments.next() else {
+        tracing::warn!(file_url = file_meta.url,
+                       "Bad file meta URL, no segments");
+        return Ok(());
+    };
+
+    if first.len() > 0 {
+        tracing::warn!(file_url = file_meta.url,
+                       "Bad file meta URL, missing initial '/'");
+        return Ok(());
+    }
+
+    // TODO: Use lazy_static!
+    let segment_re = Regex::new(r"^[-a-z_0-9A-Z.]+$").expect("parse regex");
+
+    for segment in rel_segments {
+        if !segment_re.is_match(segment) {
+            tracing::warn!(file_meta.url,
+                           file_segment = segment,
+                           "Bad file meta URL, segment didn't match regex");
+            return Ok(());
+        }
+
+        if segment == "." || segment == ".." {
+            tracing::warn!(file_meta.url,
+                           file_segment = segment,
+                           "Bad file meta URL, segment was '.' or '..'");
+            return Ok(());
+        }
+    }
+
+    let url =
+        format!("{mirror_url}{file_rel_url}",
+                // TODO: Make mirror_url a CLI argument
+                mirror_url = "https://ftp.acc.umu.se/mirror/wikimedia.org/dumps",
+                file_rel_url = file_meta.url);
+
+    let file_name = file_meta.url.split('/').last()
+        .expect("already verified segments is not empty");
+    let file_out_path = out_dir.join(format!("{dump_name}/{ver}/{job_name}/{file_name}"));
+    let file_out_dir_path = file_out_path.parent().expect("file_out_path.parent() not None");
+    std::fs::create_dir_all(&*file_out_dir_path)?;
+
+    tracing::info!(
+        url,
+        out_path = %file_out_path.display(),
+        expected_len = file_meta.size,
+        "download_job_file starting");
+
+    let mut file = tokio::fs::OpenOptions::new()
+        .write(true)
+        .create_new(true) // TODO truncate on CLI argument `--overwrite`
+        .open(file_out_path)
+        .await?;
+
+    let download_res = client.get(url.clone())
+        .send()
+        .await?;
+    let download_res_code = download_res.status();
+    let download_res_code_int = download_res_code.as_u16();
+    let download_res_code_str = download_res_code.canonical_reason().unwrap_or("");
+    tracing::debug!(url = url.clone(),
+                   response_code = download_res_code_int,
+                   response_code_str = download_res_code_str,
+                   "download job file status");
+
+    if !download_res_code.is_success() {
+        return Err(anyhow::Error::msg(
+            format!("HTTP response error downloading job file url={url} response_code={download_res_code_int} response_code_str={download_res_code_str}")));
+    }
+
+    let mut bytes_stream = download_res.bytes_stream();
+    let mut sha1_hasher = Sha1::new();
+
+    while let Some(chunk) = bytes_stream.next().await {
+        let chunk = chunk
+            .with_context(|| format!("while downloading a job file url={url}"))?;
+        tracing::trace!(chunk_len = chunk.len(), "download_job_file chunk");
+        sha1_hasher.update(&chunk);
+        tokio::io::copy(&mut chunk.as_ref(), &mut file).await?;
+    }
+
+    file.flush().await?;
+    file.sync_all().await?;
+
+    let finishing_len = file.metadata().await?.len();
+
+    drop(file);
+
+    tracing::debug!("download_job_file download complete");
+
+    if finishing_len != file_meta.size {
+        return Err(anyhow::Error::msg(format!("Download job file was the wrong size url={url} expected_len={expected} finishing_len={finishing_len}", expected = file_meta.size)))
+    }
+
+    let sha1_hash = sha1_hasher.finalize();
+    let sha1_hash_hex = hex::encode(sha1_hash);
+
+    match file_meta.sha1.as_ref() {
+        None => tracing::warn!(url, "No SHA1 hash given for job file"),
+        Some(expected_sha1) => {
+            let expected_sha1 = expected_sha1.to_lowercase();
+            if sha1_hash_hex != expected_sha1 {
+                return Err(anyhow::Error::msg(
+                    format!("Bad SHA1 hash for downloaded job file url={url} expected_sha1={expected_sha1}, computed_sha1={computed_sha1}",
+                            computed_sha1 = sha1_hash_hex)));
+            }
+
+            tracing::debug!(sha1 = expected_sha1,
+                            "Downloaded file SHA1 hash matched the expected value");
+        }
+    }
+
+    Ok(())
 }
